7b8f2282ff8fb1aaed476af3e64e08f1cb6cf536
RhineGST <GST49151709884@outlook.com>
2023-10-30 20:29:28 +0800

    FTT/NTT

--- template_FFT.cpp
+++ template_FFT.cpp
@@ -1,15 +1,28 @@
-//
-// Created by GST49 on 2023/10/30.
-//
 #include <bits/stdc++.h>
 const int G = 3, Gi = 332748118, MOD = 998244353;
 
 using ll = long long;
 using std::vector, std::swap, std::cin, std::cout, std::endl;
 
-struct Poly : public std::vector<long long> {
+struct Complex {
+    double real = 0, imag = 0;
 
-    using std::vector<long long>::vector;
+    Complex operator+ (const Complex &b) const {
+        return {real + b.real, imag + b.imag};
+    }
+
+    Complex operator- (const Complex &b) const {
+        return {real - b.real, imag - b.imag};
+    }
+
+    Complex operator* (const Complex &b) const {
+        return {real * b.real - imag * b.imag, real * b.imag + imag * b.real};
+    }
+};
+
+struct Poly : public std::vector<Complex> {
+
+    using std::vector<Complex>::vector;
 
     bool operator<(const Poly &b) const {
         return size() > b.size();
@@ -30,28 +43,28 @@ ll power(ll a, ll b = MOD - 2, ll p = MOD) {
     return res;
 }
 
-void NTT(Poly &A, vector<int> &R, int lim, int type) {
+void FFT(Poly &A, vector<int> &R, int lim, int type) {
     for (int i = 0; i < lim; i++) {
         if (i < R[i]) {
             swap(A[i], A[R[i]]);
         }
     }
     for (int mid = 1; mid < lim; mid <<= 1) {
-        ll wn = power(type == 1 ? G : Gi, (MOD - 1) / (mid << 1));
+        Complex wn(cos(M_PI / mid), type * sin(M_PI / mid));
         for (int len = mid << 1, pos = 0; pos < lim; pos += len) {
-            long long w = 1;
-            for (int k = 0; k < mid; ++k, w = w * wn % MOD) {
-                long long x = A[pos + k];
-                long long y = w * A[pos + mid + k] % MOD;
-                A[pos + k] = (x + y) % MOD;
-                A[pos + mid + k] = (x - y + MOD) % MOD;
+            Complex w(1.0, 0.0);
+            for (int k = 0; k < mid; ++k, w = w * wn) {
+                Complex x = A[pos + k];
+                Complex y = w * A[pos + mid + k];
+                A[pos + k] = x + y;
+                A[pos + mid + k] = x - y;
             }
         }
     }
     if (type == -1) {
-        long long inv = power(lim, MOD - 2);
+        Complex inv(1.0 / lim, 0.0);
         for (int i = 0; i < lim; i++) {
-            A[i] = (A[i] * inv % MOD + MOD) % MOD;
+            A[i] = A[i] * inv;
         }
     }
 }
@@ -68,27 +81,13 @@ Poly operator*(Poly A, Poly B) {
     for (int i = 1; i < lim; i++) {
         R[i] = (R[i >> 1] >> 1) | ((i & 1) << (lim_bit - 1));
     }
-    NTT(A, R, lim, 1);
-    NTT(B, R, lim, 1);
-    Poly res(lim, 0);
+    FFT(A, R, lim, 1);
+    FFT(B, R, lim, 1);
+    Poly res(lim);
     for (int i = 0; i < lim; i++) {
-        res[i] = A[i] * B[i] % MOD;
+        res[i] = A[i] * B[i];
     }
-    NTT(res, R, lim, -1);
+    FFT(res, R, lim, -1);
     res.resize(sz);
     return res;
-}
-
-// 求原根
-template<int P>
-constexpr ll findPrimitiveRoot() {
-    ll i = 2;
-    int k = __builtin_ctz(P - 1);
-    while (true) {
-        if (power(i, (P - 1) / 2, P) != 1) {
-            break;
-        }
-        i += 1;
-    }
-    return power(i, (P - 1) >> k, P);
 }
